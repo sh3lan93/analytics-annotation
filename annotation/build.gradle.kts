d038fcd988915fb6b2f2a911bde2569e206a727c
shalan <mohamed.sh3lan.93@gmail.com>
2025-09-14 14:36:44 +0400

    Feat: Configure Maven publishing for annotation module
    This commit configures the `annotation` module for publishing to Maven Central.
    
    Key changes:
    - Added the `com.vanniktech.maven.publish` plugin.
    - Set the `group` to `dev.moshalan` and `version` to `1.0.0`.
    - Configured the `mavenPublishing` block with:
        - Coordinates: `dev.moshalan:easy-analytics-annotation:1.0.0`
        - POM details: Project name, description, URL, license (Apache 2.0), developer information, and SCM information.
        - Instructions to publish to Maven Central and sign all publications.

--- annotation/build.gradle.kts
+++ annotation/build.gradle.kts
@@ -1,10 +1,13 @@
 
-
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
     id("org.jlleitschuh.gradle.ktlint")
+    id("com.vanniktech.maven.publish")
 }
+
+group = "dev.moshalan"
+version = "1.0.0"
 
 ktlint {
     reporters {
@@ -19,4 +22,42 @@ java {
 
 kotlin {
     jvmToolchain(17)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(group.toString(), "easy-analytics-annotation", version.toString())
+
+    pom {
+        name.set("Easy-Analytics")
+        description.set(
+            "A powerful, annotation-based tracking library for Android that eliminates " +
+                "boilerplate code by automatically injecting analytics tracking at " +
+                "compile time using bytecode transformation.",
+        )
+        url.set("https://github.com/sh3lan93/analytics-annotation")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("shalan")
+                name.set("Mohamed Shalan")
+                email.set("mohamed.sh3lan.93@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/sh3lan93/analytics-annotation")
+            developerConnection.set("scm:git:ssh://github.com/sh3lan93/analytics-annotation")
+            url.set("https://github.com/sh3lan93/analytics-annotation")
+        }
+    }
 }
